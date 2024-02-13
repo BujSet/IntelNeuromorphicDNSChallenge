be8b9168155069379e65f93bf5b8abf3ffb285c4
BujSet <ranganath1000@gmail.com>
2024-02-13 12:02:14 -0600

    Added plot formatting

--- hrtfs/plot_results.py
+++ hrtfs/plot_results.py
@@ -32,14 +32,11 @@ for i in range(len(onlyfiles)):
 
     speech = int(name_tokens[-4])
     noise = int(name_tokens[-2])
-    print("Speech="+str(speech) + ",noise="+str(noise))
     with open(path, 'r') as F:
         lines = F.readlines()
         tokens = lines[-1].split()
         score = float(tokens[-2])
-        dists[i] = (abs(speech - noise) * 5.625)
-        if (dists[i] > 180.0):
-            dists[i] = dists[i] - 360.0
+        dists[i] = ((speech - noise) * 5.625)
 
         if (score > maxScore):
             maxScore = score
@@ -57,7 +54,7 @@ for i in range(len(onlyfiles)):
 trendline = dict()
 for n in range(50):
     for s in range(50):
-        diff = abs(n - s)
+        diff = (s - n) 
         if channel0[n, s] < minScore or channel0[n, s] > maxScore:
             continue
         if diff in trendline.keys():
@@ -65,24 +62,37 @@ for n in range(50):
         else:
             trendline[diff] = [channel0[n,s]]
 updated = dict()
+medians = dict()
 for k, v in trendline.items():
     dist = k * 5.625
-    if (dist > 180.0):
-        dist = dist - 360.0
     val = 1.0 * sum(v) / len(v)
     updated[dist] = val
+    v.sort()
+    if (len(v) % 2 == 1):
+        medians[dist] = v[(len(v) - 1)// 2] 
+    else:
+        if (len(v) == 2):
+            medians[dist] = (0.5*v[0])  + (0.5*v[1])
+        else:
+            medians[dist] = (0.5*v[len(v)// 2])  + (0.5*v[(len(v)// 2) + 1])
 updated = sorted(updated.items())
+medians = sorted(medians.items())
 xs = [k for k,v in updated]
 ys = [v for k,v in updated]
+mxs = [k for k,v in medians]
+mys = [v for k,v in medians]
 
 fig, axs = plt.subplots(1, 2, figsize=(40,15))
 axs0, axs1 = axs
-axs0.scatter(dists, ch0Scores, color='blue')
-axs0.plot(xs, ys, color='red')
-axs0.set_xlabel("Absolute Elevation Difference (deg)")
+axs0.scatter(dists, ch0Scores, color='blue', s=1)
+axs0.plot(xs, ys, color='red', label='Mean')
+axs0.plot(mxs, mys, color='green', label='Median')
+axs0.legend()
+axs0.set_xlabel("Elevation Difference (Speech - Noise)")
+axs0.set_xticks(range(-315, 316, 45))
+axs0.set_xticklabels([str(deg) + "°" for deg in range(-315, 316, 45)])
 axs0.set_ylabel("Denoised SI-SNR (dB)")
-axs0.set_title("Impact of Spatial Separation")
-axs0.set_xlim(-180, 180)
+axs0.set_xlim(-315, 315)
 np.seterr(divide='ignore', invalid='ignore', over='ignore')
 masked_array = np.ma.masked_where(channel0 < minScore, channel0)
 masked_array = np.ma.masked_where(channel0 > maxScore, masked_array)
@@ -90,8 +100,12 @@ cmap = matplotlib.cm.hot
 cmap.set_bad('blue')
 im = axs1.imshow(masked_array, cmap=cmap, vmin=minScore, vmax=maxScore)
 axs1.set_xlim(0, 49)
+axs1.set_xticks(range(0, 49, 8))
+axs1.set_xticklabels([str(deg)  + "°\nfront" for deg in range(-45, 89, 45)] + ["90°\nup"] +  [str(deg) + "°\nback" for deg in range(45, -90, -45)])
+axs1.set_yticks(range(0, 49, 8))
+axs1.set_yticklabels(["front " + str(deg)  + "°" for deg in range(-45, 89, 45)] + ["up 90°"] +  ["back " + str(deg) + "°" for deg in range(45, -90, -45)])
 axs1.set_ylim(0, 49)
 axs1.set_ylabel("Speech Orientation")
 axs1.set_xlabel("Noise Orientation")
-fig.colorbar(im, ax=axs1)
+fig.colorbar(im, ax=axs1, label="SI-SNR (dB)")
 plt.savefig("sensitivity.png", bbox_inches="tight")
