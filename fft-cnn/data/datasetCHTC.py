f5e44acb6dbf4a6b18b18cc205a82f8e878c20bc
rklock <rklock@submit3.chtc.wisc.edu>
2024-03-20 12:24:04 -0500

    chtc files

---
+++ fft-cnn/data/datasetCHTC.py
@@ -0,0 +1,57 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import torchaudio
+
+class NoiseDataset(Dataset):
+    def __init__(self, root: str = './'):
+        """Aduio dataset loader for fft-cnn.
+
+        Parameters
+        ----------
+        root : str, optional
+            Path of the dataset location, by default './'.
+        """
+        self.root = root
+        self.noisy_files = glob.glob(root + 'noisy/**.wav')
+
+        self.file_paths = file_paths
+        self.base_path = base_path
+
+     def _get_filenames(self, n: int) -> Tuple[str, str]:
+        noisy_file = self.noisy_files[n % self.__len__()]
+        filename = noisy_file.split(os.sep)[-1]
+        file_id = int(self.file_id_from_name.findall(filename)[0])
+        noise_file = self.root + f'noise/noise_fileid_{file_id}.wav'
+       
+        return noisy_file, noise_file
+
+    def __len__(self):
+        return len(self.noisy_files)
+
+    def __getitem__(self, idx):
+        noisy_file, noise_file = self._get_filenames(n)
+
+        waveform, sample_rate = torchaudio.load(noise_file)
+
+        # Preprocess the audio - STFT, magnitude, and phase
+        n_fft = 2048  # Number of FFT components
+        hop_length = 512  # Number of samples between successive frames
+        window = torch.hann_window(n_fft)
+
+        stft = torch.stft(waveform, n_fft=n_fft, hop_length=hop_length, window=window, return_complex=True)
+        magnitude = torch.abs(stft)
+        phase = torch.angle(stft)
+
+        phase_path = path + "_phase.pt"
+        torch.save(phase, phase_path)
+        # Concatenate magnitude and phase along the last dimension
+        input_data = torch.cat((magnitude.unsqueeze(-1), phase.unsqueeze(-1)), dim=-1)
+
+        return input_data
+   
+    if __name__ == '__main__':
+        train_set = NoiseDataset(
+            root='../../data/MicrosoftDNS_4_ICASSP/training_set/')
+        validation_set = NoiseDataset(
+            root='../../data/MicrosoftDNS_4_ICASSP/validation_set/')
---
+++ fft-cnn/model/train-chtc.py
@@ -0,0 +1,128 @@
+# model/train.py
+import torch
+import torch.nn as nn
+import argparse
+from torch.utils.data import DataLoader
+import sys
+sys.path.append('./')
+
+import glob
+from torch.utils.tensorboard import SummaryWriter
+
+from nn import PhaseShiftCNN
+from datasetCHTC import NoiseDataset
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gpu',
+                        type=int,
+                        default=[0],
+                        help='which gpu(s) to use', nargs='+')
+    parser.add_argument('-b',
+                        type=int,
+                        default=32,
+                        help='batch size for dataloader')
+    parser.add_argument('-lr',
+                        type=float,
+                        default=0.001,
+                        help='initial learning rate')
+    parser.add_argument('-epoch',
+                        type=int,
+                        default=50,
+                        help='number of epochs to run')
+    parser.add_argument('-path',
+                        type=str,
+                        default='../../',
+                        help='dataset path')
+
+    args = parser.parse_args()
+    trained_folder = 'Trained' + identifier
+    logs_folder = 'Logs' + identifier
+    print(trained_folder)
+    
+    writer = SummaryWriter('runs/' + identifier)
+    os.makedirs(trained_folder, exist_ok=True)
+    os.makedirs(logs_folder, exist_ok=True)
+
+    with open(trained_folder + '/args.txt', 'wt') as f:
+        for arg, value in sorted(vars(args).items()):
+            f.write('{} : {}\n'.format(arg, value))
+
+    print('Using GPUs {}'.format(args.gpu))
+    device = torch.device('cuda:{}'.format(args.gpu[0]))
+    
+    out_delay = args.out_delay
+    if len(args.gpu) == 0:
+        print("Building CPU network")
+        net = PhaseShiftCNN().to(device)
+
+        module = net
+    else:
+        print("Building GPU network")
+        net = torch.nn.DataParallel(PhaseShiftCNN().to(device),
+                                    device_ids=args.gpu)
+        
+        module = net.module
+    
+    optimizer = torch.optim.Adam(net.parameters(),
+                                lr = args.lr)
+    
+    train_set = NoiseDataset(root=args.path + 'training_set/')
+    validation_set = NoiseDataset(root=args.path + 'validation_set/')
+    
+    train_loader = DataLoader(train_set, 
+                              batch_size=args.b,
+                              shuffle=True,
+                              num_workers=4,
+                              pin_memory=True)
+    
+    validation_loader = DataLoader(validation_set, 
+                              batch_size=args.b,
+                              shuffle=True,
+                              num_workers=4,
+                              pin_memory=True)
+
+     for epoch in range(args.epoch):
+        if (epoch < 5 or epoch % 5 == 0):
+            print(f"Entering epoch: {epoch}")
+        
+        for batch_idx, data in enumerate(train_loader):
+            net.train()
+            
+            inputs = data.to(device) 
+            
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            
+            targets = torch.zeros_like(outputs).to(device) # maybe this works
+            loss = loss_function(outputs, targets)
+            
+            loss.backward() 
+            optimizer.step()
+        
+        for batch_idx, data in enumerate(validation_loader):
+            net.eval()
+            
+            inputs = data.to(device) 
+            outputs = net(inputs)
+            targets = torch.zeros_like(outputs).to(device) # maybe this works
+            loss = loss_function(outputs, targets)
+        
+        writer.add_scalar('Loss/train', stats.training.loss, epoch)
+        writer.add_scalar('Loss/valid', stats.validation.loss, epoch)
+        
+        stats.update()
+        stats.plot(path=trained_folder + '/')
+        
+        if stats.validation.best_accuracy is True:
+            torch.save(module.state_dict(), trained_folder + '/fft-cnn.pt')
+
+        stats.save(trained_folder + '/')
+    
+    module.load_state_dict(torch.load(trained_folder + '/fft-cnn.pt'))
+    module.export_hdf5(trained_folder + '/fft-cnn.net')
+    params_dict = {}
+    for key, val in args._get_kwargs():
+        params_dict[key] = str(val)
+    writer.flush()
+    writer.close()
