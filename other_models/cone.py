2a08cae7c1c297c237c7cb0c61152dc232673bd7
BujSet <ranganath1000@gmail.com>
2024-06-13 22:11:51 -0500

    Seems like cone model now works, testing long running

--- other_models/cone.py
+++ other_models/cone.py
@@ -76,7 +76,7 @@ class ConeFilter(torch.nn.Module):
         self.register_buffer('freq_map', freq_map, persistent=False)
         self.c = 343 # Speed of sound in air m/s
         self.rho = 1.293 # Air density kg/m^3
-        self.steps = 100
+        self.steps = 1
         self.pi = 3.1415927
 
     def forward(self, noisy):
@@ -87,8 +87,12 @@ class ConeFilter(torch.nn.Module):
         mydepth = torch.relu(self.depth) # depth must be non-negative
         myangle = ((torch.sigmoid(self.angle) + 1.0)/2.0) * (self.pi/2.0)
 
+        # myx0 = self.x0
+        # mydepth = self.depth
+        # myangle = ((self.angle + 1.0)/2.0) * (self.pi/2.0)
         for i in range(self.steps):
             x = (myx0 + (i * mydepth/self.steps)).float()
+            # x = (myx0).float()
             r = torch.tan(myangle) * x
             numer = torch.mul(self.freq_map, x)
             denom = torch.add(numer, self.c)
@@ -103,168 +107,25 @@ class ConeFilter(torch.nn.Module):
         tot_impedance = tot_impedance.unsqueeze(1)
         tot_impedance = tot_impedance.unsqueeze(0)
         result = torch.mul(result, tot_impedance)
-        self.print_parameters()
+        # self.print_parameters()
         return result
 
     def print_parameters(self):
-        print("x0: " + str(self.x0.detach().item()))
-        print("angle: " + str(self.angle.detach().item() * self.pi / 180.0))
-        print("depth: " + str(self.depth.detach().item()))
+        print("x0: " + str(self.x0.item()))
+        print("angle (degrees): " + str(self.angle.item() * self.pi / 180.0))
+        print("depth: " + str(self.depth.item()))
 
 class Network(torch.nn.Module):
-    def __init__(self, 
-            threshold=0.1, 
-            tau_grad=0.1, 
-            scale_grad=0.8, 
-            max_delay=64, 
-            out_delay=0):
+    def __init__(self):
         super().__init__()
-        self.stft_mean = 0.2
-        self.stft_var = 1.5
-        self.stft_max = 140
-        self.out_delay = out_delay
-        self.EPS = 2.220446049250313e-16
-
-        sigma_params = { # sigma-delta neuron parameters
-            'threshold'     : threshold,   # delta unit threshold
-            'tau_grad'      : tau_grad,    # delta unit surrogate gradient relaxation parameter
-            'scale_grad'    : scale_grad,  # delta unit surrogate gradient scale parameter
-            'requires_grad' : False,  # trainable threshold
-            'shared_param'  : True,   # layer wise threshold
-        }
-        sdnn_params = {
-            **sigma_params,
-            'activation'    : F.relu, # activation function
-        }
-
-        self.input_quantizer = lambda x: slayer.utils.quantize(x, step=1 / 64)
-
-        self.blocks = torch.nn.ModuleList([
-            ConeFilter(),
-            slayer.block.sigma_delta.Input(sdnn_params),
-            slayer.block.sigma_delta.Dense(sdnn_params, 257, 512, weight_norm=False, delay=True, delay_shift=True),
-            slayer.block.sigma_delta.Dense(sdnn_params, 512, 512, weight_norm=False, delay=True, delay_shift=True),
-            slayer.block.sigma_delta.Output(sdnn_params, 512, 257, weight_norm=False),
-        ])
-
-        self.blocks[1].pre_hook_fx = self.input_quantizer
-
-        self.blocks[2].delay.max_delay = max_delay
-        self.blocks[3].delay.max_delay = max_delay
-
-    def _display_weights(self, filename=None):
-        layerIndices = []
-        # First determine which layers have actual weights
-        vmin =  100000.0
-        vmax = -100000.0
-        for i in range(len(self.blocks)):
-            block = self.blocks[i]
-            if (hasattr(block, 'synapse')):
-                if (hasattr(block.synapse, 'weight')):
-                    layerIndices.append(i)
-                    mini = block.synapse.weight.cpu().detach().min()
-                    maxi = block.synapse.weight.cpu().detach().max()
-                    if (mini < vmin):
-                        vmin = mini
-                    if (maxi > vmax):
-                        vmax = maxi
-            if (hasattr(block, 'print_parameters')):
-                block.print_parameters()
-
-        if (filename == None):
-            return
-
-        fig, axs = plt.subplots(1, len(layerIndices), figsize=(30,20))
-
-        for i in range(len(layerIndices)):
-            idx = layerIndices[i]
-            block = self.blocks[idx]
-            assert(hasattr(block, 'synapse') and hasattr(block.synapse, 'weight'))
-            w = block.synapse.weight.cpu().squeeze().detach().numpy().transpose()
-            pad = np.zeros((max(w.shape), max(w.shape)))
-            m = w.reshape((w.shape[0], -1))
-            pad[0:m.shape[0], 0:m.shape[1]] = m
-            im = axs[i].imshow(pad, cmap='hot', interpolation='nearest', aspect='auto', vmin=vmin, vmax=vmax)
-            axs[i].set_title('Layer ' + str(i) + ' Weights')
-        axs[1].set_xlabel("Output Neurons")
-        axs[0].set_ylabel("Input Neurons")
-        fig.subplots_adjust(right=0.8)
-        cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-        fig.colorbar(im, cax=cbar_ax)
-        plt.savefig(filename, bbox_inches='tight')
-        plt.close()
+        self.blocks = torch.nn.ModuleList([ConeFilter()])
 
     def forward(self, noisy):
-
         cone = self.blocks[0]
-        noisy = cone(noisy)
-
-        x = noisy - self.stft_mean
-
-        for i in range(1, len(self.blocks)):
-            block = self.blocks[i]
-            x = block(x)
-
-        mask = torch.relu(x + 1)
-        return slayer.axon.delay(noisy, self.out_delay) * mask
-
-    def grad_flow(self, path):
-        # helps monitor the gradient flow
-        grad = [b.synapse.grad_norm for b in self.blocks if hasattr(b, 'synapse')]
-
-        plt.figure()
-        plt.semilogy(grad)
-        plt.savefig(path + 'gradFlow.png')
-        plt.close()
-
-        return grad
-
-    def validate_gradients(self):
-        valid_gradients = True
-        for name, param in self.named_parameters():
-            if param.grad is not None:
-                valid_gradients = not (torch.isnan(param.grad).any()
-                                       or torch.isinf(param.grad).any())
-                if not valid_gradients:
-                    break
-        if not valid_gradients:
-            self.zero_grad()
-
-    def export_hdf5(self, filename):
-        # network export to hdf5 format
-        h = h5py.File(filename, 'w')
-        layer = h.create_group('layer')
-        for i, b in enumerate(self.blocks):
-            b.export_hdf5(layer.create_group(f'{i}'))
-
-        
-def nop_stats(dataloader, stats, sub_stats, print=True):
-    t_st = datetime.now()
-    for i, (noisy, clean, noise) in enumerate(dataloader):
-        with torch.no_grad():
-            noisy = noisy
-            clean = clean
-
-            score = si_snr(noisy, clean)
-            sub_stats.correct_samples += torch.sum(score).item()
-            sub_stats.num_samples += noisy.shape[0]
-
-            processed = i * dataloader.batch_size
-            total = len(dataloader.dataset)
-            time_elapsed = (datetime.now() - t_st).total_seconds()
-            samples_sec = time_elapsed / (i + 1) / dataloader.batch_size
-            header_list = [f'Train: [{processed}/{total} '
-                           f'({100.0 * processed / total:.0f}%)]']
-            if print:
-                stats.print(0, i, samples_sec, header=header_list)
-
+        return cone(noisy)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-gpu',
-                        type=int,
-                        default=[0],
-                        help='which gpu(s) to use', nargs='+')
     parser.add_argument('-b',
                         type=int,
                         default=32,
@@ -277,26 +138,10 @@ if __name__ == '__main__':
                         type=float,
                         default=0.001,
                         help='lagrangian factor')
-    parser.add_argument('-threshold',
-                        type=float,
-                        default=0.1,
-                        help='neuron threshold')
-    parser.add_argument('-tau_grad',
-                        type=float,
-                        default=0.1,
-                        help='surrogate gradient time constant')
-    parser.add_argument('-scale_grad',
-                        type=float,
-                        default=0.8,
-                        help='surrogate gradient scale')
     parser.add_argument('-n_fft',
                         type=int,
                         default=512,
                         help='number of FFT specturm, hop is n_fft // 4')
-    parser.add_argument('-dmax',
-                        type=int,
-                        default=64,
-                        help='maximum axonal delay')
     parser.add_argument('-out_delay',
                         type=int,
                         default=0,
@@ -313,7 +158,7 @@ if __name__ == '__main__':
                         type=int,
                         default=None,
                         help='random seed of the experiment')
-    parser.add_argument('-training_epoch',
+    parser.add_argument('-epochs',
                         type=int,
                         default=50,
                         help='number of training epochs to run')
@@ -336,8 +181,6 @@ if __name__ == '__main__':
     assert(args.spectrogram == 0 or args.spectrogram == 1 or args.spectrogram == 2)
     trained_folder = 'Trained' + identifier
     logs_folder = 'Logs' + identifier
-    # print(trained_folder)
-#    writer = SummaryWriter('runs/' + identifier)
 
     os.makedirs(trained_folder, exist_ok=True)
     os.makedirs(logs_folder, exist_ok=True)
@@ -347,30 +190,11 @@ if __name__ == '__main__':
             f.write('{} : {}\n'.format(arg, value))
 
     lam = args.lam
-
-    print('Using GPUs {}'.format(args.gpu))
-    device = torch.device('cuda:{}'.format(args.gpu[0]))
-
+    device = torch.device('cuda:0')
     out_delay = args.out_delay
-    # if len(args.gpu) == 0:
-    #     print("Building CPU network")
-    #     net = Network(args.threshold,
-    #                   args.tau_grad,
-    #                   args.scale_grad,
-    #                   args.dmax,
-    #                   args.out_delay).to(device)
-    #     module = net
-    # else:
-    print("Building GPU network")
-    net = Network(
-                args.threshold,
-                args.tau_grad,
-                args.scale_grad,
-                args.dmax,
-                args.out_delay)
-    net = torch.nn.DataParallel(net.to(device), device_ids=args.gpu)
+    print("Building GPU network using GPU 0")
+    net = torch.nn.DataParallel(Network().to(device), device_ids=[0])
     module = net.module
-    # module._display_weights("initial_weights.png")
     stft_transform =torchaudio.transforms.Spectrogram(
                 n_fft=args.n_fft,
                 onesided=True, 
@@ -410,42 +234,32 @@ if __name__ == '__main__':
                                    num_workers=4,
                                    pin_memory=True)
 
-    base_stats = slayer.utils.LearningStats(accuracy_str='SI-SNR',
-                                            accuracy_unit='dB')
-
-    stats = slayer.utils.LearningStats(accuracy_str='SI-SNR',
-                                       accuracy_unit='dB')
-
-    print("Training for " + str(args.training_epoch) + " epochs")
-    training_st = datetime.now()
-    for epoch in range(args.training_epoch):
-        t_st = datetime.now()
-        batch_st = datetime.now()
-        batch_tot = 0.0
-        conv_tot = 0.0
-        synth_tot = 0.0
+    print("Training Cone Model for " + str(args.epochs) + " epochs")
+    print("Parameters are initially:...")
+    module.blocks[0].print_parameters()
+    print("")
+    print("")
+    for epoch in range(args.epochs):
         for i, (noisy, clean, noise, idx) in enumerate(train_loader):
-            batch_tot += (datetime.now() - batch_st).total_seconds()
             net.train()
 
-            ssl_noise = noise.to(device)
-            ssl_noisy = noisy.to(device)
-            ssl_clean = clean.to(device)
+            noisy = noisy.to(device)
+            clean = clean.to(device)
 
             if (args.spectrogram == 0):
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, None)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, None)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, None)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, None)
             elif(args.spectrogram == 1):
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, stft_transform)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, stft_transform)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, stft_transform)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, stft_transform)
             else:
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, mel_transform)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, mel_transform)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, mel_transform)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, mel_transform)
 
             denoised_abs = net(noisy_abs)
             noisy_arg = slayer.axon.delay(noisy_arg, out_delay)
             clean_abs = slayer.axon.delay(clean_abs, out_delay)
-            clean = slayer.axon.delay(ssl_clean, args.n_fft // 4 * out_delay)
+            clean = slayer.axon.delay(clean, args.n_fft // 4 * out_delay)
 
             if (args.spectrogram == 0):
                 clean_rec = stft_mixer(denoised_abs, noisy_arg, args.n_fft, None)
@@ -463,58 +277,33 @@ if __name__ == '__main__':
 
             optimizer.zero_grad()
             loss.backward()
-            module.validate_gradients()
+
             torch.nn.utils.clip_grad_norm_(net.parameters(), args.clip)
             optimizer.step()
 
             if torch.isnan(score).any():
                 score[torch.isnan(score)] = 0
-
-            stats.training.correct_samples += torch.sum(score).item()
-            stats.training.loss_sum += loss.item()
-            stats.training.num_samples += noisy.shape[0]
-
-            processed = i * train_loader.batch_size
-            total = len(train_loader.dataset)
-            time_elapsed = (datetime.now() - t_st).total_seconds()
-            samples_sec = time_elapsed / (i + 1) / train_loader.batch_size
-            header_list = [f'Train: [{processed}/{total} '
-                          f'({100.0 * processed / total:.0f}%)]']
-            stats.print(epoch, i, samples_sec, header=header_list)
-            batch_st = datetime.now()
-
-#        writer.add_scalar('Loss/train', stats.training.loss, epoch)
-#        writer.add_scalar('SI-SNR/train', stats.training.accuracy, epoch)
-        stats.update()
-    print("")
-    training_time = (datetime.now() - training_st).total_seconds()
-    print("Training time: " + str(training_time))
-    print("Mini batch fill time: " + str(batch_tot))
-    print("Convolution time: " + str(conv_tot))
-    print("Synth time: " + str(synth_tot))
-    print("")
-    # module._display_weights()
-    with open("validation_orients.txt", 'w') as vof:
-        print("Speech Orientation, Noise Orientation, Validation Accuracy", file=vof)
-    validation_st = datetime.now()
-
-    t_st = datetime.now()
+            # statString = "Train [" + str(epoch) + " | " + str(i) + "] -> "
+            # statString += str(loss.item()) + " " 
+            # statString += str(torch.mean(score).item()) + " SI-SNR dB"
+            # print(statString)
+    module.blocks[0].print_parameters()
     for i, (noisy, clean, noise, idx) in enumerate(validation_loader):
         net.eval()
         with torch.no_grad():
 
-            ssl_noisy = noisy.to(device)
-            ssl_clean = clean.to(device)
+            noisy = noisy.to(device)
+            clean = clean.to(device)
             
             if (args.spectrogram == 0):
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, None)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, None)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, None)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, None)
             elif(args.spectrogram == 1):
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, stft_transform)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, stft_transform)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, stft_transform)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, stft_transform)
             else:
-                noisy_abs, noisy_arg = stft_splitter(ssl_noisy, args.n_fft, mel_transform)
-                clean_abs, clean_arg = stft_splitter(ssl_clean, args.n_fft, mel_transform)
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft, mel_transform)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft, mel_transform)
 
             denoised_abs = net(noisy_abs)
             noisy_arg = slayer.axon.delay(noisy_arg, out_delay)
@@ -533,42 +322,8 @@ if __name__ == '__main__':
             loss = lam * F.mse_loss(denoised_abs, clean_abs) + (100 - torch.mean(score))
             if torch.isnan(loss).any():
                 loss[torch.isnan(loss)] = 0
-            stats.validation.correct_samples += torch.sum(score).item()
-            stats.validation.loss_sum += loss.item()
-            stats.validation.num_samples += noisy.shape[0]
 
-            processed = i * validation_loader.batch_size
-            total = len(validation_loader.dataset)
-            time_elapsed = (datetime.now() - t_st).total_seconds()
-            samples_sec = time_elapsed / (i + 1) / validation_loader.batch_size
-            header_list = [f'Valid: [{processed}/{total} ' f'({100.0 * processed / total:.0f}%)]']
-            stats.print(epoch, i, samples_sec, header=header_list)
-#        writer.add_scalar('Loss/valid', stats.validation.loss, epoch)
-#        writer.add_scalar('SI-SNR/valid', stats.validation.accuracy, epoch)
-#        stats.testing.update()
-#        stats.testing.reset()
-
-    # print("")
-    # print("")
-    # print("")
-    # validation_time = (datetime.now() - validation_st).total_seconds()
-    # print("Validation time: " + str(validation_time))
-    # print("Mini batch fill time: " + str(batch_tot))
-    # print("Convolution time: " + str(conv_tot))
-    # print("Synth time: " + str(synth_tot))
-    # module._display_weights("final_weights.png")
-#        stats.plot(path=trained_folder + '/')
-#        if stats.validation.best_accuracy is True:
-#            torch.save(module.state_dict(), trained_folder + '/network.pt')
-#        stats.save(trained_folder + '/')
-
-    # torch.save(module.state_dict(), trained_folder + '/network.pt')
-    # module.load_state_dict(torch.load(trained_folder + '/network.pt'))
-#    module.export_hdf5(trained_folder + '/network.net')
-
-#    params_dict = {}
-#    for key, val in args._get_kwargs():
-#        params_dict[key] = str(val)
-#    writer.add_hparams(params_dict, {'SI-SNR': stats.validation.max_accuracy})
-#    writer.flush()
-#    writer.close()
+            statString = "Valid [" + str(epoch) + " | " + str(i) + "] -> "
+            statString += str(loss.item()) + " " 
+            statString += str(torch.mean(score).item()) + " SI-SNR dB"
+            print(statString)
