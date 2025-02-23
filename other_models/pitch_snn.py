9f6013f64524371ba9669b94a609fe7028cd4aa2
BujSet <ranganath1000@gmail.com>
2025-02-23 12:31:17 -0600

    Update to make sweep files easier to ID

--- other_models/pitch_snn.py
+++ other_models/pitch_snn.py
@@ -555,4 +555,4 @@ if __name__ == '__main__':
                 'scheduler_state_dict': scheduler.state_dict(),
                 'tracking_info': trackingInfo,
                 'command_line_args': args,
-                }, trained_folder + '/pitch_snn_depth_' +str(args.hiddenLayers) + "_"  + str(epochsCompleted) + '.pt')
+                }, trained_folder + '/pitch_snn_' + args.exp + '.pt')
