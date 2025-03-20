7c6a79313dc56ae0a954f2b7d75d474b8363f41a
BujSet <ranganath1000@gmail.com>
2025-03-20 08:45:21 -0500

    Adding some debugging info to help with tests

--- audio_dataloader.py
+++ audio_dataloader.py
@@ -19,6 +19,8 @@ class DNSAudioAndCrepeCleanOnly:
         self.root = root
         self.clean_files = glob.glob(root + 'clean/**.wav')
         self.crepe_files = glob.glob(root + 'crepe_pitch_annotations/**.csv')
+        print("Number of clean wav files: " + str(len(self.clean_files)))
+        print("Number of crepe csv files: " + str(len(self.crepe_files)))
         self.file_id_from_name = re.compile('fileid_(\d+)')
         if (maxFiles > len(self.clean_files)):
             print("Too many files to subsample dataset "+ str(maxFiles) + "/" + str(len(self.clean_files)))
