f15b589da97931f2cc32d236b640f2313d7234d8
BujSet <ranganath1000@gmail.com>
2025-03-28 03:09:07 -0500

    Another minor bug fix

--- other_models/pitch_validate_comparisons.py
+++ other_models/pitch_validate_comparisons.py
@@ -37,11 +37,10 @@ def stft_splitter(audio, n_fft=512, method=None):
 
 def crepe_collate_pitch_estimation(fft_centers, times, values, confs, threshold=-1.0):
     # TODO use times to create appropriate arrays
-    freq = torch.from_numpy(values).float()
     if threshold >= 0.0:
-        clippedFreq = torch.where(confs >= threshold, freq, 0.0)
+        clippedFreq = torch.where(confs >= threshold, values, 0.0)
         return clippedFreq
-    return freq
+    return values
 
 def freq_to_one_hot(value, freq_bins):
     one_hot = torch.zeros(len(freq_bins))
