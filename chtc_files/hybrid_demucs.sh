b3b40b092728a2afd2ec0b1ec265146b63138ea5
BujSet <ranganath1000@gmail.com>
2025-04-10 12:12:37 -0500

    Updates to CHTC files to run hybrid demucs on full MUSDB dataset

--- chtc_files/hybrid_demucs.sh
+++ chtc_files/hybrid_demucs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
 # hybrid_demucs.sh
-# My CHTC job
 #
 export MPLCONFIGDIR=$(pwd)
 export TORCH_EXTENSIONS_DIR=$(pwd)
 export HOME=$(pwd)
-python3 hybrid_demucs_tutorial.py #
+git clone https://github.com/BujSet/IntelNeuromorphicDNSChallenge.git -q
+cd IntelNeuromorphicDNSChallenge/
+export INTEL_NDNS_HOME=$(pwd)
+git switch elise_branch -q
+git submodule init -q
+git submodule update -q
+touch null_condor_config
+export CONDOR_CONFIG=$(pwd)/null_condor_config
+mv ~/musdb18hq.zip .
+unzip musdb18hq.zip
+python3 chtc_files/ hybrid_demucs_full_dataset.py
