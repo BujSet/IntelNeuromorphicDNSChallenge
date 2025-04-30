9b7cbcb88dda5b0c109d9bb01ffb4609f34b6d2c
BujSet <ranganath1000@gmail.com>
2025-04-29 23:28:25 -0500

    Updated plotting script to start making charts for paper

--- inspection/plot_passive_pinna_results.py
+++ inspection/plot_passive_pinna_results.py
@@ -68,6 +68,19 @@ def fill_row_average(dest, data):
             dest[i] = -1.0
     return dest
 
+def make_3D_plot(ax, data3D, azimuth, prefix):
+    scatterAX = ax.scatter(x, y, z, c=data3D, cmap=cm.hot)
+    ax.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
+    ax.set_yticks([0.5, -0.5], labels=["Right", "Left"])
+    ax.set_zticks([0.5, -0.5], labels=["Below", "Above"])
+    tmp_planes = ax.zaxis._PLANES
+    ax.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
+                             tmp_planes[0], tmp_planes[1],
+                             tmp_planes[4], tmp_planes[5])
+    ax.view_init(elev=ELEV, azim=azimuth)
+    ax.set_title(prefix+" θ="+str(azimuth)+"°", fontweight='bold')
+    return scatterAX
+
 Subject3 = CipicDatabase.subjects[3]
 cart_pos = Subject3.getCartesianPositions()
 cart_pos[:,0] = -1.0 * cart_pos[:, 0]
@@ -86,154 +99,27 @@ fill_row_average(row_average, data)
 
 ELEV=25
 
-fig = plt.figure(figsize=(20,12))
-ax1 = fig.add_subplot(2,4,1, projection='3d')
-scatter1 = ax1.scatter(x, y, z, c=col_average, cmap=cm.hot)
+fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(25,12), subplot_kw={'projection': '3d'})
+scatter1 = make_3D_plot(axes[0,0], row_average, 45, "a)")
+make_3D_plot(axes[0,1], row_average, 135, "b)")
+make_3D_plot(axes[0,2], row_average, 225, "c)")
+make_3D_plot(axes[0,3], row_average, 315, "d)")
+fig.text(0.5, 0.91, 'Speech Audio-sphere', ha='center', fontsize=14, fontweight='bold')
 
-# Set labels
-ax1.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax1.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax1.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax1.zaxis._PLANES
-ax1.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax1.view_init(elev=ELEV, azim=45)
-ax1.set_title("θ=45°")
-
-ax2 = fig.add_subplot(2,4,2, projection='3d')
-scatter2 = ax2.scatter(x, y, z, c=col_average, cmap=cm.hot)
-ax2.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax2.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax2.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax2.zaxis._PLANES
-ax2.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax2.view_init(elev=ELEV, azim=135)
-ax2.set_title("θ=135°")
-
-ax3 = fig.add_subplot(2,4,3, projection='3d')
-scatter3 = ax3.scatter(x, y, z, c=col_average, cmap=cm.hot)
-ax3.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax3.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax3.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax3.zaxis._PLANES
-ax3.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax3.view_init(elev=ELEV, azim=225)
-ax3.set_title("θ=225°")
-
-ax4 = fig.add_subplot(2,4,4, projection='3d')
-ax4.scatter(x, y, z, c=col_average, cmap=cm.hot)
-ax4.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax4.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax4.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax4.zaxis._PLANES
-ax4.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax4.view_init(elev=ELEV, azim=315)
-ax4.set_title("θ=315°")
-
-#fig.text(0.5, 0.95, 'Noise AudioSphere', ha='center', fontsize=14)
-
-ax5 = fig.add_subplot(2,4,5, projection='3d')
-scatter5 = ax5.scatter(x, y, z, c=row_average, cmap=cm.hot)
-ax5.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax5.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax5.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax5.zaxis._PLANES
-ax5.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax5.view_init(elev=ELEV, azim=45)
-ax5.set_title("θ=45°")
-
-ax6 = fig.add_subplot(2,4,6, projection='3d')
-scatter6 = ax6.scatter(x, y, z, c=row_average, cmap=cm.hot)
-ax6.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax6.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax6.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax6.zaxis._PLANES
-ax6.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax6.view_init(elev=ELEV, azim=135)
-ax6.set_title("θ=135°")
-
-ax7 = fig.add_subplot(2,4,7, projection='3d')
-scatter7 = ax7.scatter(x, y, z, c=row_average, cmap=cm.hot)
-ax7.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax7.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax7.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax7.zaxis._PLANES
-ax7.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax7.view_init(elev=ELEV, azim=225)
-ax7.set_title("θ=225°")
-
-ax8 = fig.add_subplot(2,4,8, projection='3d')
-scatter8 = ax8.scatter(x, y, z, c=row_average, cmap=cm.hot)
-ax8.set_xticks([0.5, -0.5], labels=["Dorsal", "Ventral"])
-ax8.set_yticks([0.5, -0.5], labels=["Right", "Left"])
-ax8.set_zticks([0.5, -0.5], labels=["Below", "Above"])
-tmp_planes = ax8.zaxis._PLANES
-ax8.zaxis._PLANES = (tmp_planes[2], tmp_planes[3],
-                             tmp_planes[0], tmp_planes[1],
-                             tmp_planes[4], tmp_planes[5])
-ax8.view_init(elev=ELEV, azim=315)
-ax8.set_title("θ=315°")
-
-#ax5 = fig.add_subplot(3,3,5, projection='3d')
-#ax5.scatter(x, y, z, c=col_average, cmap=cm.hot)
-#ax5.set_xlabel('X')
-#ax5.set_ylabel('Y')
-#ax5.set_zlabel('Z')
-#ax5.view_init(elev=30, azim=200)
-#ax5.set_title("Elev 30, Azim 200")
-
-#ax6 = fig.add_subplot(3,3,6, projection='3d')
-#ax6.scatter(x, y, z, c=col_average, cmap=cm.hot)
-#ax6.set_xlabel('X')
-#ax6.set_ylabel('Y')
-#ax6.set_zlabel('Z')
-#ax6.view_init(elev=30, azim=240)
-#ax6.set_title("Elev 30, Azim 240")
-
-#ax7 = fig.add_subplot(3,3,7, projection='3d')
-#ax7.scatter(x, y, z, c=col_average, cmap=cm.hot)
-#ax7.set_xlabel('X')
-#ax7.set_ylabel('Y')
-#ax7.set_zlabel('Z')
-#ax7.view_init(elev=30, azim=280)
-#ax7.set_title("Elev 30, Azim 280")
-
-#ax8 = fig.add_subplot(3,3,8, projection='3d')
-#ax8.scatter(x, y, z, c=col_average, cmap=cm.hot)
-#ax8.set_xlabel('X')
-#ax8.set_ylabel('Y')
-#ax8.set_zlabel('Z')
-#ax8.view_init(elev=30, azim=320)
-#ax8.set_title("Elev 30, Azim 320")
-
-#ax9 = fig.add_subplot(3,3,9, projection='3d')
-#scatter9 = ax9.scatter(x, y, z, c=col_average, cmap=cm.hot)
-#ax9.set_xlabel('X')
-#ax9.set_ylabel('Y')
-#ax9.set_zlabel('Z')
-#ax9.view_init(elev=30, azim=360)
-#ax9.set_title("Elev 30, Azim 360")
+fig.text(0.5, 0.50, 'Noise Audio-sphere', ha='center', fontsize=14, fontweight='bold')
+make_3D_plot(axes[1,0], col_average, 45, "e)")
+make_3D_plot(axes[1,1], col_average, 135, "f)")
+make_3D_plot(axes[1,2], col_average, 225, "g)")
+make_3D_plot(axes[1,3], col_average, 315, "h)")
 
 #fig.colorbar(scatter9)
 # Create an inset axes for the colorbar
-cbax = fig.add_axes([0.15, 0.05, 0.7, 0.03])  # [left, bottom, width, height]
+cbax = fig.add_axes([0.11, 0.1, 0.78, 0.02])  # [left, bottom, width, height]
+fig.text(0.5, 0.07, 'Final SI-NSR (dB)', ha='center', fontsize=14, fontweight='bold')
 
 # Add the colorbar to the inset axes
 cbar = fig.colorbar(scatter1, cax=cbax, orientation='horizontal')
-plt.savefig("passive_pinna_sub3_chan0_3D_noise.png", bbox_inches="tight")
+plt.savefig("passive_pinna_sub3_chan0_3D.pdf", bbox_inches="tight", format='pdf')
 plt.close()
 
 plt.figure(figsize=(25, 25))
