238c6136d3f6ce027d5ee86b2b96399ef8c6c65a
Elise Song <elise.song@wisc.edu>
2026-09-13 21:32:48 -0500

    chtc run with cipic

--- chtc_files/musdb_hq_snn_training.sh
+++ chtc_files/musdb_hq_snn_training.sh
@@ -17,4 +17,17 @@ touch null_condor_config
 export CONDOR_CONFIG=$(pwd)/null_condor_config
 mv ~/musdb18hq.zip .
 unzip -q musdb18hq.zip
+
+cd hrtfs/cipic/
+echo "[CIPIC] Initiating tarball copy" \
+        && mv ~/cipic_dataset.tar.gz . \
+        && echo "[CIPIC] Tarball copy success.. Initiating tarball unpack" \
+        && tar -xzf cipic_dataset.tar.gz \
+        && echo "[CIPIC] Tarball unpack success... Initiating tarball removal" \
+        && rm -f cipic_dataset.tar.gz \
+        && echo "[CIPIC] Tarball removal success" \
+        && mv cipic/*.sofa . \
+        && rm -rf cipic/
+cd ../../
+
 python3 chtc_files/musdb_snn_training.py -path . "$@"
